ea6054a39f66d132f48435cc2463d0554f1fe74e
PatriiSerrano <patricia.serranomolina@gmail.com>
2024-07-01 11:53:20 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,39 +1,96 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
 
-const PaperElement = document.getElementById('paper');
-const ScissorsElement = document.getElementById('scissors');
-const RockElement = document.getElementById('rock');
+const gameItemsElement = document.getElementById('game-items');
+/* const userElectionElement = document.getElementById('user-picked');
+const computerElectionElement = document.getElementById('computer-picked'); */
+const gameResultElement = document.getElementById('game-results');
+const pointsUserElement = document.getElementById('points-user');
+const pointsComputerElement = document.getElementById('points-pc');
 
-let userElection = '';
+const options = ['rock', 'paper', 'scissors'];
 
-function play(userElection) {
-  // Definir las opciones disponibles por medio de un Arreglo
-  let options = [RockElement, PaperElement, ScissorsElement];
+let userElection = null;
+let computerElection = null;
+let userPoints = 0;
+let computerPoints = 0;
 
-  // Generar la elección aleatoria de la máquina con metodos
-  let computerElection = options[Math.floor(Math.random() * options.length)];
+// Objeto con las reglas del juego
+const gameOptions = {
+  rock: {
+    scissors: true,
+    lizard: true,
+    paper: false,
+    spock: false,
+  },
+  scissors: {
+    paper: true,
+    lizard: true,
+    rock: false,
+    spock: false,
+  },
+  paper: {
+    rock: true,
+    spock: true,
+    scissors: false,
+    lizard: false,
+  },
+  lizard: {
+    rock: true,
+    spock: true,
+    scissors: false,
+    paper: false,
+  },
+  spock: {
+    rock: true,
+    scissors: true,
+    paper: false,
+    lizard: false,
+  },
+};
 
-  // Verificar el resultado del juego con condicional if
+//Puntuaciones
+const updateScore = () => {
+  pointsUserElement.textContent = userPoints;
+  pointsComputerElement.textContent = computerPoints;
+};
+
+//Empate, Ganar, Perder
+const checkWinner = () => {
+  console.log(userElection + ' - ' + computerElection);
   if (userElection === computerElection) {
-    // Empate
-    console.log('Empate. Ambos eligieron ' + eleccionUsuario + '.');
-  } else if (
-    (userElection === RockElement && computerElection === ScissorsElement) ||
-    (userElection === PaperElement && computerElection === RockElement) ||
-    (userElection === ScissorsElement && computerElection === PaperElement)
-  ) {
-    // El usuario gana
-    console.log('¡Ganaste! Tú eliges ' + userElection + ' y la máquina elige ' + computerElection + '.');
-  } else {
-    // El usuario pierde
-    console.log('Perdiste. Tú eliges ' + userElection + ' y la máquina elige ' + computerElection + '.');
+    console.log('Empate');
+    return;
   }
-}
 
-// Verificar si la elección del usuario es válida y jugar el juego
-if (userElection === 'rock' || userElection === 'paper' || userElection === 'scissors') {
-  play(userElection);
-} else {
-  console.log('Elección inválida. Por favor, elige rock, paper o scissors.');
-}
+  if (gameOptions[userElection][computerElection]) {
+    console.log('WIN');
+    userPoints++;
+  } else {
+    console.log('LOSE');
+    computerPoints++;
+  }
+
+  updateScore();
+};
+
+//Elección del ordenador
+const getcomputerSelection = () => {
+  const randomNumber = Math.floor(Math.random() * options.length);
+  computerElection = options[randomNumber];
+  checkWinner();
+  //console.log(computerElection);
+};
+
+//Elección del usuario
+const getuserSelection = selection => {
+  userElection = selection;
+  getcomputerSelection();
+  //console.log(userElection);
+};
+
+// Escuchar el objeto que he pulsado
+gameItemsElement.addEventListener('click', event => {
+  if (!event.target.classList.contains('game-item')) return;
+  getuserSelection(event.target.dataset.item);
+});
