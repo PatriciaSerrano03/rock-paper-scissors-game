1cc17e34e3c5cd979868b324bf81548d55d70328
PatriiSerrano <patricia.serranomolina@gmail.com>
2024-07-02 09:54:14 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,12 @@
 import '../scss/styles.scss';
 
 const gameItemsElement = document.getElementById('game-items');
-/* const userElectionElement = document.getElementById('user-picked');
-const computerElectionElement = document.getElementById('computer-picked'); */
 const gameResultElement = document.getElementById('game-results');
 const pointsUserElement = document.getElementById('points-user');
 const pointsComputerElement = document.getElementById('points-pc');
+const playAgainElement = document.getElementById('play-again');
+const modalElement = document.getElementById('modal');
+const buttonRulesElement = document.getElementById('button-rules');
 
 const options = ['rock', 'paper', 'scissors'];
 
@@ -14,6 +15,7 @@ let userElection = null;
 let computerElection = null;
 let userPoints = 0;
 let computerPoints = 0;
+//let showResults = false;
 
 // Objeto con las reglas del juego
 const gameOptions = {
@@ -47,6 +49,15 @@ const gameOptions = {
     paper: false,
     lizard: false,
   },
+};
+
+//Objeto Imágenes
+const gameImages = {
+  rock: './assets/images/icon-rock.svg',
+  paper: './assets/images/icon-paper.svg',
+  scissors: './assets/images/icon-scissors.svg',
+  lizard: './assets/images/icon-lizard.svg',
+  spock: './assets/images/icon-spock.svg',
 };
 
 //Puntuaciones
@@ -94,3 +105,59 @@ gameItemsElement.addEventListener('click', event => {
   if (!event.target.classList.contains('game-item')) return;
   getuserSelection(event.target.dataset.item);
 });
+
+/* //Enseñar Modal
+const showModal = () => {
+  modalElement.classList.add('modal--show');
+};
+
+//Ocultar Modal
+const hideModal = () => {
+  modalElement.classList.remove('modal--show');
+};
+
+//Aparecen los elementos escondidos en advanced
+if (document.body.dataset.mode === 'advanced') {
+  gameOptions.push('lizard', 'spock');
+} */
+
+/* //Cambiar la pantalla de los resultados
+const changeScreen = () => {
+  if (showResults) {
+    gameItemsElement.classList.add('hide');
+    gameResultElement.classList.remove('hide');
+  } else {
+    gameItemsElement.classList.remove('hide');
+    gameResultElement.classList.add('hide');
+  }
+};
+
+const printResultsImage = () => {
+  const userOldClass = pickedUserImageElement.parentElement.classList[2];
+  const pcOldClass = pickedPcImageElement.parentElement.classList[2];
+
+  pickedUserImageElement.src = gameImages[userSelection];
+  pickedPcImageElement.src = gameImages[pcSelection];
+
+  if (userOldClass && pcOldClass) {
+    pickedUserImageElement.parentElement.classList.replace(userOldClass, `game-item--${userSelection}`);
+    pickedPcImageElement.parentElement.classList.replace(pcOldClass, `game-item--${pcSelection}`);
+  } else {
+    pickedUserImageElement.parentElement.classList.add(`game-item--${userSelection}`);
+    pickedPcImageElement.parentElement.classList.add(`game-item--${pcSelection}`);
+  }
+
+  showResults = true;
+
+  changeScreen();
+};
+
+playAgainElement.addEventListener('click', () => {
+  showResults = false;
+  changeScreen();
+});
+
+buttonRulesElement.addEventListener('click', showModal);
+
+modalElement.addEventListener('click', hideModal);
+ */
